da858f608b8af0439cd3dc432f9894c34f048e40
Josh <joshmessitte@gmail.com>
2021-11-24 14:24:43 -0500

    Implement Logout

--- app/src/main/java/edu/uga/cs/csci4060_rideshare/MainActivity.java
+++ app/src/main/java/edu/uga/cs/csci4060_rideshare/MainActivity.java
@@ -2,13 +2,37 @@ package edu.uga.cs.csci4060_rideshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logOutBtn;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logOutBtn = findViewById(R.id.logout);
+
+        auth = FirebaseAuth.getInstance();
+
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent( v.getContext(), SignIn.class );
+                v.getContext().startActivity( intent );
+
+            }
+        });
+
     }
 }
