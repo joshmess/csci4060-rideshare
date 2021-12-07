24f04bc2812da73d97bcc06c477600c05d347c22
jefferyjohn <jefferyjohn2000@gmail.com>
2021-12-07 08:21:19 -0500

    user rides on account details

--- app/src/main/java/edu/uga/cs/csci4060_rideshare/AccountDetails.java
+++ app/src/main/java/edu/uga/cs/csci4060_rideshare/AccountDetails.java
@@ -2,20 +2,37 @@ package edu.uga.cs.csci4060_rideshare;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountDetails extends AppCompatActivity {
 
 
     private TextView email;
     private FirebaseAuth auth;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter recyclerAdapter;
+    private List<Ride> rideList;
+    private String app_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,39 @@ public class AccountDetails extends AppCompatActivity {
             // No user is signed in
         }
         email.setText(userEmail);
+
+        // Display accepted rides
+        recyclerView = (RecyclerView) findViewById( R.id.recyclerView );
+        layoutManager = new LinearLayoutManager(this );
+        recyclerView.setLayoutManager( layoutManager );
+        rideList = new ArrayList<Ride>();
+
+        // get a Firebase DB instance reference
+        app_user = userEmail;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(app_user + "-rides");
+
+        myRef.addListenerForSingleValueEvent( new ValueEventListener() {
+
+            @Override
+            public void onDataChange( DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, knowing that this is a list,
+                // we need to iterate over the elements and place them on a List.
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    Ride ride = postSnapshot.getValue(Ride.class);
+                    rideList.add(ride);
+                }
+
+                // Now, create a JobLeadRecyclerAdapter to populate a RecyclerView to display the job leads.
+                recyclerAdapter = new RideRecyclerAdapter( rideList );
+                recyclerView.setAdapter( recyclerAdapter );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        } );
 
     }
 }
